4760b44f2ad7ca08a8dcf847d855bf67dfa54b15
mutazhkabashi <mutazhkabbashi@gmail.com>
2021-03-11 16:11:46 +0300

    lab3

---
+++ database_handler.py
@@ -0,0 +1,196 @@
+import sqlite3
+from flask import g
+from uuid import uuid4
+import random
+
+
+DATABASE_URI = "./twidder"      # Mutaz's Machine Path
+#DATABASE_URI = "D:\\WebDevelopment\\WebLab\\Lab3\\lab3\\tddd97\\lab3\\twidder" # Talha's Machine Path
+
+
+
+def get_db():
+    db = getattr(g, 'db', None)
+    if db is None:
+        db = g.db = sqlite3.connect(DATABASE_URI)
+    return db
+
+
+def disconnect_db():
+    db = getattr(g, 'db', None)
+    if db is not None:
+        g.db.close()
+        g.db = None
+
+
+def sign_in(email, password):
+    currentPassword = get_db().execute(
+        "SELECT password FROM User WHERE email =?;", [email])
+    currentPassword = convert_cursor_with_single_value_to_string(
+        currentPassword)
+    currentPassword = str(currentPassword)
+    print("password & current password",currentPassword," ",password)
+    if password == currentPassword:
+        rand_token = str(uuid4())
+        rand_token = generate_token() #rand_token[0:rand_token.find("-")]
+        print("token ", rand_token)
+        status = "logedIn"
+        get_db().execute("insert into UserSession(token,user_email,status) values (?,?,?);",
+               (rand_token, email, status))
+        get_db().commit()
+        return rand_token
+    else:
+        raise Exception('Incorrect Password')
+
+
+def sign_up(email, first_name, last_name, gender, city, country, password):
+    get_db().execute("insert into User (email, first_name, last_name, gender, city, country, password) values (?,?,?,?,?,?,?)",
+           (email, first_name, last_name, gender, city, country, password))
+    get_db().commit()
+    return True
+
+
+def sign_out(userToken):
+    user_status = get_db().execute(
+        "SELECT status FROM UserSession WHERE token =?;", [userToken])
+    user_status = convert_cursor_with_single_value_to_string(user_status)
+    user_status = str(user_status)
+    if str(user_status) == 'logedIn':
+        user_status = 'logedOut'
+        get_db().execute("UPDATE UserSession SET status ='" +
+               user_status+"' where token ='"+userToken+"';")
+        get_db().commit()
+        return True
+    else:
+        print("logIn First")
+        return False
+
+
+def change_password(userToken, oldPassword, newPassword):
+    user_status = get_db().execute(
+        "SELECT status FROM UserSession WHERE token =?;", [userToken])
+    user_status = convert_cursor_with_single_value_to_string(user_status)
+    user_status = str(user_status)
+    if str(user_status) == 'logedIn':
+        currentPassword = get_db().execute(
+            "SELECT password FROM User u inner join UserSession s on s.user_email=u.email WHERE token =?;", [userToken])
+        currentPassword = convert_cursor_with_single_value_to_string(
+            currentPassword)
+        currentPassword = str(currentPassword)
+        
+        userEmail = get_db().execute(
+            "SELECT email FROM User u inner join UserSession s on s.user_email=u.email WHERE token =?;", [userToken])
+        userEmail = convert_cursor_with_single_value_to_string(
+            userEmail)
+        userEmail = str(userEmail)
+
+        if oldPassword == currentPassword:
+            try:
+                get_db().execute("UPDATE User SET password=? where email=?;", [newPassword,userEmail])
+                get_db().commit()
+                return "success"
+            except Exception as e:
+                return False
+        else:
+            return "password is incorrect"
+    else:
+        return "Login First"
+
+
+def get_user_data_by_token(userToken):
+    user_status = check_token_validaty(userToken)
+    if str(user_status) == 'logedIn':
+        user_profile = get_db().execute(
+            "SELECT u.* FROM User u inner join UserSession s on u.email = s.user_email  WHERE token =?;", [userToken])
+        return user_profile.fetchall()
+    elif str(user_status) == 'logedOut':
+        print("logIn First")
+        raise Exception("invalid token / logIn First")
+
+def get_user_data_by_email(userToken, userEmail):
+    user_status = check_token_validaty(userToken)
+    if str(user_status) == 'logedIn':
+        user_data = get_db().execute("SELECT u.email,u.first_name, u.last_name, u.city, u.country, u.gender FROM User u WHERE u.email =?;", [userEmail])
+        return user_data.fetchall()
+    else:
+        raise Exception("invalid token / logIn First")
+
+
+
+def get_user_message_by_token(userToken):
+    user_status = check_token_validaty(userToken)
+    if str(user_status)=='logedIn':
+        user_message = get_db().execute("SELECT p.post_message FROM Post p inner join UserSession s on p.user_email = s.user_email  WHERE token=?;", [userToken])
+        return user_message.fetchall()
+    else:
+        raise Exception("invalid token / logIn First")
+
+def get_user_message_by_email(userToken,userEmail):
+    user_status = check_token_validaty(userToken)
+    print("status ",user_status)
+    if str(user_status)=='logedIn':
+        user_message = get_db().execute("SELECT post_message FROM Post WHERE user_email =?;", [userEmail])
+        return user_message.fetchall()
+    else:
+        raise Exception("invalid token / logIn First")
+
+
+def post_message(userToken, email,post_message,):
+    user_status = get_db().execute("SELECT status FROM UserSession WHERE token=? and user_email=?;", [userToken,email])        
+    user_status = convert_cursor_with_single_value_to_string(user_status)
+    user_status = str(user_status)
+    if user_status == 'logedIn':
+        get_db().execute("INSERT INTO Post(user_email,post_message) values(?,?);", [email,post_message])
+        get_db().commit()
+        return True;
+    else:
+        raise Exception("invalid token / logIn First")
+
+def get_user_email_by_token(userToken):
+    cursor = get_db().execute("SELECT user_email FROM UserSession where status = 'logedIn' and  token = ?", [userToken])
+    user = cursor.fetchone()
+    cursor.close()
+    if  user == None:
+        return False
+    else:
+        return user[0]
+
+
+# util methods
+
+def convert_cursor_with_single_value_to_string(cursor):
+    fetch = cursor.fetchall()
+    if len(str(fetch))==2:
+        return "invalid token"
+    result = str(fetch[0])
+    result=str(result)[2:(len(result)-3)]
+    return result.strip()
+
+def check_token_validaty(token):
+    token_status = get_db().execute("SELECT status FROM UserSession WHERE token =?;", [token])
+    token_status = convert_cursor_with_single_value_to_string(token_status)
+    token_status = str(token_status)
+    return token_status
+
+def generate_token():
+    TOKEN_LENGTH = 20
+    TOKEN_STRING = "abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ"
+    token = ""
+
+    for i in range(0, TOKEN_LENGTH):
+        token += TOKEN_STRING[random.randint(0, len(TOKEN_STRING)-1)]
+
+    return token
+
+def get_session_status_by_email(userEmail):
+    token_status = get_db().execute("SELECT status FROM UserSession WHERE status='logedIn' and user_email =?;", [userEmail])
+    token_status = convert_cursor_with_single_value_to_string(token_status)
+    token_status = str(token_status)
+    print("Token status ",token_status)
+    if(token_status !="invalid token"):
+        return True
+    return False
+
+def deactivate_session_status(userEmail):
+    get_db().execute("UPDATE UserSession SET status='logedOut' where user_email=?;", [userEmail])
+    get_db().commit()
---
+++ static/client.js
@@ -0,0 +1,435 @@
+var userToken;
+var userEmail;
+var webSocket = null;
+
+
+function onPageLoad(){
+  console.log("Token "+localStorage.getItem("userToken"));
+   if(localStorage.getItem("userToken")=="undefined"  || localStorage.getItem("userToken")=="" || localStorage.getItem("userToken")==null){
+    document.getElementById("page-content").innerHTML = document.getElementById("welcomeview").innerHTML;
+   }
+    else{
+     connectToSocket();
+     document.getElementById("page-content").innerHTML = document.getElementById("profileview").innerHTML;
+     document.getElementById('Home').style.display = "block";
+     getProfileInfoByToken();
+   } 
+}
+
+// welcome page functions
+function login(formData){
+  console.log(formData.loginPassword.value);
+  /* var */ userEmail = formData.loginEmail.value
+  localStorage.setItem("userEmail",userEmail);
+  var password = formData.loginPassword.value;
+  genericPostXHR("/signin", JSON.stringify({'email' : userEmail, 'password' : password}), loginSuccess,loginFail);
+  
+}
+
+function loginFail(result){
+  console.log("faillogin "+JSON.stringify(result.status));
+  var statusCode = result.status;
+  if(statusCode =="401")
+  showMessage("signin-errorMessageLabel","signin-errorMessage","Error : ","Wrong username or password.");
+}
+
+function loginSuccess(result){
+  console.log("login success "+JSON.stringify(JSON.parse(result.responseText)));
+  var resultData = JSON.parse(result.responseText);
+  console.log("resultdata token "+resultData['token']);
+  userToken = resultData['token'];
+  localStorage.setItem("userToken",userToken);
+  console.log("token "+userToken);
+  document.getElementById("login-form").reset();
+  document.getElementById("page-content").innerHTML = document.getElementById("profileview").innerHTML;
+  document.getElementById('Home').style.display = "block";
+  getProfileInfoByToken();
+  //createSocketConnection();
+  connectToSocket();
+  
+
+}
+
+function register(formData){
+    var email = formData.signupEmail.value
+    var password = formData.signupPassword.value;
+    var confirmPassword =  formData.signupRepeatPassword.value;
+    var firstName = formData.signupFirstName.value;
+    var familyName = formData.signupFamilyName.value;
+    var gender = formData.signupGender.value;
+    var city = formData.signupCity.value;
+    var country = formData.signupCountry.value;
+
+
+
+    var result = validatePasswordAndConfirmPassword(password,confirmPassword);
+    if(result == false){
+      showMessage("signup-errorMessageLabel","signup-errorMessage","Error :","Password and ConfirmPassword Don't Match");
+    }
+    else{
+      var newUserData ={
+        "email": email,
+        "password": password,
+        "firstname":firstName,
+        "familyname":familyName,
+        "gender":gender,
+        "city":city,
+        "country":country
+      };
+      genericPostXHR("/signup", JSON.stringify(newUserData), signupSuccess,signupFail);
+    }
+}
+
+function signupFail(result){
+  //console.log("faillogin "+JSON.stringify(statusCode));
+  var statusCode = result.status;
+  if(statusCode =="400"){
+    showMessage("signup-errorMessageLabel","signup-errorMessage","Error : ","Form data missing or incorrect type.");
+  }
+  else if(statusCode =="409"){
+  showMessage("signup-errorMessageLabel","signup-errorMessage","Error : ","Email is already exist");
+  }
+  else if(statusCode =="500"){
+    showMessage("signup-errorMessageLabel","signup-errorMessage","Error : ","Something went wrong");
+    }
+
+}
+function signupSuccess(result){
+  var statusCode = result.status;
+  if(statusCode=="201"){
+  showMessage("signup-errorMessageLabel","signup-errorMessage","Success : ","Successfully created a new user.");
+  document.getElementById("signup-form").reset();
+  }
+}
+
+//Main page functions
+function getProfileInfoByToken(){
+  genericGetXHR("/getuserdatabytoken", JSON.stringify(""), getProfileInfoByTokenSuccess,getProfileInfoByTokenFail);
+}
+
+function getProfileInfoByTokenSuccess(result){
+  //console.log("response "+JSON.parse(result.responseText));
+  userData = JSON.parse(result.responseText);
+  //console.log("user Data "+JSON.stringify(userData));
+  setUserProfile(userData);
+  getPostedMessages();
+}
+
+function getProfileInfoByTokenFail(status){
+
+}
+
+function getProfileInfoByTokenAndEmail(){
+  //FIXME when should i populate useremail variable
+  var userData = serverstub.getUserDataByEmail(userToken,userEmail);
+  console.log("user Data "+JSON.stringify(userData));
+  setUserProfile(userData);
+  
+}
+
+function setUserProfile(userData){
+  var email = userData['email'];
+  var firstName = userData['first_name'];
+  var familyName = userData['last_name'];
+  var gender = userData['gender'];
+  var city = userData['City'];
+  var country = userData['Country'];
+
+  //
+   if(!document.getElementById("home_f_name")){
+    document.getElementById("page-content").innerHTML = document.getElementById("profileview").innerHTML;
+    document.getElementById('Home').style.display = "block";
+   }
+  // home user profile
+  document.getElementById("home_f_name").innerHTML = email;
+  console.log("user profile "+email);
+  document.getElementById("home_l_name").innerHTML = firstName;
+  document.getElementById("home_i_email").innerHTML = familyName;
+  document.getElementById("home_i_gender").innerHTML = gender;
+  document.getElementById("home_i_country").innerHTML = city;
+  document.getElementById("home_i_city").innerHTML = country;
+
+}
+
+//TODO
+// function getUserMessageByToken(){
+//   var userMessages = serverstub.getUserMessageByToken(localStorage.getItem("userToken"));
+
+// }
+//TODO
+// function getUserMessageByTokenAndEmail(){
+//   var userMessages = serverstub.getUserMessageByToken(localStorage.getItem("userToken"),userEmail);
+
+// }
+
+function postMessage(formData){
+  var message = formData.homePostText.value;
+  genericPostXHR("/postmessage", JSON.stringify({"email":localStorage.getItem("userEmail"),"message":message}), postMessageSuccess,postMessageFail);
+}
+
+//FIXME
+function postMessageFail(statusCode){
+
+}
+function postMessageSuccess(result){
+  var statusCode = result.status;
+  if(statusCode="201"){
+  document.getElementById("post_text").value="";
+  getPostedMessages();
+  }
+}
+
+function refresh(){
+  getPostedMessages();
+}
+
+function getPostedMessages(){
+  genericGetXHR("/getusermessagebytoken", JSON.stringify(""), getPostMessageSuccess,postMessageFail);
+}
+
+function getPostMessageSuccess(result){
+  var messages = "";
+  //console.log("response "+JSON.parse(result.responseText));
+  userMessages = JSON.parse(result.responseText);
+  //console.log("user Messages "+userMessages.length)
+  userMessages.forEach(function(entry) {
+  //console.log("entry"+JSON.stringify(entry));
+   messages +="User :"+localStorage.getItem("userEmail")+" Posted "+entry.message+" <br>";
+});
+  document.getElementById("home_previous_post").innerHTML = messages ;
+}
+
+//FIXME
+function getPostMessageFail(status){
+  
+}
+
+function getPostedMessagesByEmail(formData){
+  showMessage("browse-errorMessageLabel","browse-errorMessage","","");
+  document.getElementById("serachResult").innerHTML = "";
+  userEmail = formData.userEmail.value;
+  console.log("email "+userEmail);
+  localStorage.setItem("browseEmail",userEmail);
+  genericPostXHR("/getusermessagebyemail", JSON.stringify({'email' : userEmail}), getPostedMessagesByEmailSuccess,getPostedMessagesByEmailFail);
+
+  console.log("mesages "+JSON.stringify(userMessages));
+
+}
+
+function getPostedMessagesByEmailSuccess(result){
+  if(result.status =="204"){
+    showMessage("browse-errorMessageLabel","browse-errorMessage","Error : ","Email dose not exist");
+   return; 
+  }
+  var messages ="";
+  console.log("response "+JSON.parse(result.responseText));
+  userMessages = JSON.parse(result.responseText);
+  console.log("user Messages "+userMessages.length)
+  userMessages.forEach(function(entry) {
+  console.log("entry"+JSON.stringify(entry));
+   messages +="User :"+localStorage.getItem("browseEmail")+" Posted "+entry.message+" <br>";
+});
+  document.getElementById("serachResult").innerHTML = messages ;
+  document.getElementById("User_info1").value="";
+}
+
+//TDOD same functionly to other Fail methods should be one common method called by other methods
+function getPostedMessagesByEmailFail(result){
+  if(result.status =="401"){
+  showMessage("browse-errorMessageLabel","browse-errorMessage","Error : ","You are not signed in.");
+  }
+  if(result.status =="400"){
+    showMessage("browse-errorMessageLabel","browse-errorMessage","Error : ","data missing or email dosen't exist.");
+    }
+    document.getElementById("User_info1").value="";
+
+}
+
+//FIXME use genericGetXHR 
+function logout(){
+  //var userData = serverstub.signOut(localStorage.getItem("userToken"));
+  genericGetXHR("/signout", JSON.stringify(""), logoutSuccess,logoutFail);
+
+}
+
+function logoutFail(result){
+
+  if(result.status =="401"){
+  localStorage.setItem("userToken","");
+  document.getElementById("page-content").innerHTML = document.getElementById("welcomeview").innerHTML;
+  }
+  if(result.status =="400"){
+    showMessage("signout-errorMessageLabel","signin-errorMessage","Error : ","data missing or incorrect type.");
+    }
+}
+
+function logoutSuccess(result){
+  localStorage.setItem("userToken","");
+  document.getElementById("page-content").innerHTML = document.getElementById("welcomeview").innerHTML;
+  websocket.close();
+
+}
+
+function changePassword(formData){
+  console.log(formData.oldPassword.value);
+  var currentPassword = formData.oldPassword.value;
+  var newPassword = formData.newPassword.value;
+  var confirmNewPassword = formData.confirmNewPassword.value;
+ 
+  
+  var result = validatePasswordAndConfirmPassword(newPassword,confirmNewPassword);
+  if(result == false){
+    document.getElementById("account-changePassword-ErrorMessage").style.display ="block";
+    showMessage("changePassword-errorMessageLabel","changePassword-errorMessage","Error :","new Password and Confirm new Password Don't Match");
+  }
+  else{ // change password block
+  genericPostXHR("/changepassword", JSON.stringify({'oldPassword' : currentPassword, 'newPassword' : newPassword}), changePasswordSuccess,changePasswordFail);
+
+}
+}
+
+function changePasswordFail(result){
+  console.log("faillogin "+JSON.stringify(result.status));
+  var statusCode = result.status;
+  if(statusCode =="401")
+  showMessage("changePassword-errorMessageLabel","changePassword-errorMessage","Error : ","Wrong username or password.");
+  if(statusCode =="400"){
+    showMessage("changePassword-errorMessageLabel","changePassword-errorMessage","Error : ","Form data missing or incorrect type.");
+  }
+  if(statusCode =="500"){
+    showMessage("changePassword-errorMessageLabel","changePassword-errorMessage","Error : ","Something went wrong Try again.");
+  }
+}
+
+function changePasswordSuccess(result){
+  document.getElementById("account_change_password-form").reset();
+  // document.getElementById("account-changePassword-ErrorMessage").style.display ="block";
+  // document.getElementById("page-content").innerHTML = document.getElementById("welcomeview").innerHTML
+  // showMessage("signin-errorMessageLabel","signin-errorMessage","Success : ","Passweord Chaged Successfully");
+  logout();
+  //location.reload();
+    //websocket.close();
+
+}
+
+//util functions
+function validatePassword(password){
+    console.log(password.value.length());
+
+}
+
+function validatePasswordAndConfirmPassword(password,confirmPassword){
+    if(password/* .value */ != confirmPassword/* .value */) {
+      return false;
+      } else {
+        return true;
+      }
+}
+
+function showMessage(messageLabelId,messageTextId,messageLabel,messageText){
+    document.getElementById(messageLabelId).innerHTML = messageLabel;
+    document.getElementById(messageTextId).innerHTML = messageText;
+}
+
+
+
+
+function openTab(evt, tabName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function genericPostXHR(url, jsonValues, successCall, failureCall){
+  var xhr = new XMLHttpRequest();
+  var httpSuccessRegEx = /^2\d{2}$/
+	xhr.onreadystatechange = function(){
+    console.log("Status "+this.status);
+		if (this.readyState == 4 && httpSuccessRegEx.test(this.status) /*this.status == 200*/)
+		{
+      successCall(this);
+    }
+    else{
+      failureCall(this);
+    }
+	}
+	xhr.open("POST", url, true);
+  xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+  xhr.setRequestHeader('Token',localStorage.getItem("userToken"));
+	xhr.send(jsonValues);
+}
+
+function genericGetXHR(url, jsonValues, successCall, failureCall){
+  var xhr = new XMLHttpRequest();
+  var httpSuccessRegEx = /^2\d{2}$/
+	xhr.onreadystatechange = function(){
+    console.log("Status "+this.status);
+		if (this.readyState == 4 && /*httpSuccessRegEx.test(this.status)*/ this.status == 200)
+		{
+      successCall(this);
+    }
+    else{
+      failureCall(this);
+    }
+	}
+	xhr.open("GET", url, true);
+  xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+  console.log("token inside genericget "+localStorage.getItem("userToken"));
+  xhr.setRequestHeader('Token',localStorage.getItem("userToken"));
+	xhr.send(jsonValues);
+}
+
+// createSocketConnection = function(){
+// 	debugger;
+
+//     webSocket= new WebSocket("ws://localhost:5001/api");
+//     console.log("inside create socket connection")
+// 	 var data = JSON.stringify({'token': localStorage.getItem("userToken")});
+// 	 webSocket.onopen = function () {
+// 			 console.log(data);
+// 			 webSocket.send(data);
+// 	 };
+// 	 webSocket.onmessage = function (ev) {
+// 			 if (ev.data == "true") {
+// 					 console.log("true");
+// 					 logout();
+// 					 webSocket.close();
+// 			 }
+
+
+// 	 };
+// 	 webSocket.onclose = function (ev) {
+
+// 	 }
+// }
+function connectToSocket() {
+  let conn = new WebSocket("ws://localhost:5001/socket");
+  conn.onopen = function () {
+    let data = { "email": localStorage.getItem("userEmail"), "token": localStorage.getItem("userToken") };
+    console.log("open ws "+JSON.stringify(data));
+    conn.send(JSON.stringify(data));
+  };
+
+  conn.onmessage = function (message) {
+    message = JSON.parse(message.data);
+    console.log("message ws "+JSON.stringify(message));
+    if (message.success == false) {
+      logout();
+    }
+  };
+}
